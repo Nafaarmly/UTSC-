aa5ed63261fb3caf889c707fa72810006f54172d
Nafaarmly <111984912+Nafaarmly@users.noreply.github.com>
2022-09-28 17:35:53 +0700

    Add files via upload

---
+++ Nafa Armelya_01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    cout << "Nama : Nafa Armelya Nur Ashsyifa"<<endl;
+    cout << "Kelas : X Rpl 1"<<endl;
+    cout << "Nilai : 95"<<endl;
+ }
